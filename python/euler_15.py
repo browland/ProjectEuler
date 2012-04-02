0c00aefbc3ac2e9a372136d13adc3c7b53f65fdd
Ben Rowland <ben.rowland@gmail.com>
2012-04-02 23:09:01 +0100

    Add Problems 15 and 16

---
+++ python/euler_15.py
@@ -0,0 +1,4 @@
+import math
+
+# Simply calculate permutations with repetition
+print math.factorial(40)/(math.factorial(20)*math.factorial(20))
---
+++ python/euler_16.py
@@ -0,0 +1,3 @@
+## Kind of cheating by using Python's native bignum support. Still, a bit of a workout in functional programming.
+print sum(map(lambda x: int(x), str(pow(2,1000))))
+
